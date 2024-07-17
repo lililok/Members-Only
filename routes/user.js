ad841cdcac2b99019cf6cb62d7ad2241e81c2558
Lililok <eshanu.sofia@yandex.ru>
2024-07-17 20:19:10 +0300

    enter home page

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bcrypt = require('bcryptjs');
-const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const User = require("../models/userSchema.js");
+
+const router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.redirect("/login");
@@ -10,6 +13,22 @@ router.get('/', function(req, res, next) {
 router.get("/login", (req, res) => {
     res.render("views/login");
   });
+
+  router.get("/home", (req, res) => {
+    if (req.isAuthenticated()) {
+      res.render("views/home");
+    } else {
+      res.redirect("/login");
+    }
+  });
+
+  router.post(
+    "/login",
+    passport.authenticate("local", {
+      successRedirect: "/home",
+      failureRedirect: "/login"
+    })
+  );
 
 router.get("/sign-up", (req, res) => res.render("views/sign-up"));
 
@@ -34,4 +53,46 @@ router.post("/sign-up", async (req, res, next) => {
   }
 });
 
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username: username });
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        };
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" })
+        }
+        return done(null, user);
+      } catch(err) {
+        return done(err);
+      };
+    })
+  );
+  
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch(err) {
+      done(err);
+    };
+  });
+
+  router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
+
+
+  
 module.exports = router;
