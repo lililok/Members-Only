45ec0ed34efd16b1abfd0341c2c5f792acef0b84
Lililok <eshanu.sofia@yandex.ru>
2024-07-22 13:36:15 +0300

    minor changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,17 +2,21 @@ const asyncHandler = require("express-async-handler");
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+require('dotenv').config();
+
+const User = require("../models/userSchema.js");
 
 exports.login = asyncHandler(async (req, res, next) => {
     passport.authenticate("local", {
         successRedirect: "/home",
         failureRedirect: "/login"
-      })
+    })(req, res, next);
 })
 
 exports.home = asyncHandler(async (req, res, next) => {
     if (req.isAuthenticated()) {
-        res.render("views/home", user);
+        const user = await User.findById(req.user.id);
+        res.render("views/home", { user });
       } else {
         res.redirect("/login");
       }
@@ -38,3 +42,36 @@ exports.signup = asyncHandler(async (req, res, next) => {
         next(err);
       }
 })
+
+exports.join = asyncHandler(async (req, res, next) => {
+
+})
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+}));
+  
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 
 const user_controller = require("../controllers/userController");
-const User = require("../models/userSchema.js");
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.render("views/login"));
+router.get('/login', (req, res) => res.render("views/login"));
 router.post("/login", user_controller.login)
 
 router.get('/home', user_controller.home)
@@ -16,46 +13,16 @@ router.get('/home', user_controller.home)
 router.get("/sign-up", (req, res) => res.render("views/sign-up"));
 router.post("/sign-up", user_controller.signup);
 
-passport.use(
-    new LocalStrategy(async (username, password, done) => {
-      try {
-        const user = await User.findOne({ username: username });
-        if (!user) {
-          return done(null, false, { message: "Incorrect username" });
-        };
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-          return done(null, false, { message: "Incorrect password" })
-        }
-        return done(null, user);
-      } catch(err) {
-        return done(err);
-      };
-    })
-  );
-  
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-  
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch(err) {
-      done(err);
-    };
-  });
+router.get("/join-the-club", (req, res) => res.render("views/join-the-club"));
+router.post("/join-the-club", user_controller.join);
 
-  router.get("/logout", (req, res, next) => {
-    req.logout((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/");
-    });
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
+});
 
-
-  
 module.exports = router;
